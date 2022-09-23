fe474d47916b4c323f0d8d32db3a705c87b40dd4
maz47zu <cravx9@gmail.com>
2022-09-24 00:41:46 +0200

    reapair history data loading bug

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -7,33 +7,38 @@ import { useLocation } from 'react-router-dom'
 export default function Details() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
-  const [error, setError] = useState(null);
 
   const location = useLocation();
   const { hiveId } = location.state;
 
   const url = 'http://51.68.141.235:8088/history/hive/' + hiveId.toString();
 
-  const getHiveHistory = async() => {
-    //fetch("/api/hives")
-    //console.log('Updating...')
-    fetch(url)
-      //uncomment on localhost 
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  };
+
   useEffect(function () {
+    const getHiveHistory = async() => {
+      //fetch("/api/hives")
+      //console.log('Updating...')
+      fetch(url)
+        //uncomment on localhost 
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+          (error) => {
+            setIsLoaded(true);
+          }
+        )
+    };
     getHiveHistory();
-  }, [])
+    // eslint-disable-next-line
+  }, [setItems])
+
+  useEffect(function () {
+    console.log(items);
+  },[items]);
+
 
   if (!isLoaded || items.length===0) {
     return (
@@ -46,9 +51,9 @@ export default function Details() {
     )
   } else {
     return (
-      <Container>
+      <Container sx={{marginTop:'2.7rem'}}>
         <Grid container spacing={3}>
-          costam
+          {items[0].mother}
         </Grid>
       </Container>
     )
